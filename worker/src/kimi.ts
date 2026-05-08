d1a0e3b1d553914ef72f682dcaf530dc84d5fbc4
privatedeskai <privatedeskai@gmail.com>
2026-05-08 12:43:04 -0700

    Update kimi.ts

--- worker/src/kimi.ts
+++ worker/src/kimi.ts
@@ -1,4 +1,4 @@
-import { secp256k1 } from '@noble/curves/secp256k1';
+import { secp256k1 } from '@noble/curves/secp256k1.js';
 
 export interface IterationResult {
   python: string; summary: string; enough: boolean; reason: string;
